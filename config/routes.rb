45fac1b5dc8aa840dc714fa5f8ae09001f6d9e53
dscace <derek.scace@gmail.com>
2017-03-16 18:20:18 -0400

    Add certificate

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
 
   get 'brows' => 'criselda#brows'
 
+  get '.well-known/acme-challenge/fuhcaFO8S4_TzHhOAXV7V8Ou5CoYHwQ_PrVoksBxZp0' => 'criselda#cert'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
